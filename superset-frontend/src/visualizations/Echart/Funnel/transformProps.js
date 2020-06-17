7af890e0a4645fd14fbf7106d9ffe603faa61768
JiaQi <qi.jia@xtransfer.cn>
2020-06-17 10:53:17 +0800

    optimize loading style

--- superset-frontend/src/visualizations/Echart/Funnel/transformProps.js
+++ superset-frontend/src/visualizations/Echart/Funnel/transformProps.js
@@ -1,6 +1,5 @@
 export default function transformProps(chartProps) {
     const {width, height, queryData} = chartProps;
-    console.log(chartProps);
     return {
         data: queryData.data,
         width,
